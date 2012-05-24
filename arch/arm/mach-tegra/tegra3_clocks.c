c6170c3971d745fb6f2347ccc4ba09bddd25772e
Hunk Lin <hulin@nvidia.com>
2012-05-24 10:09:22 +0800

    Revert "Revert "arm: tegra3: change min_rate for clocks""
    This reverts commit 18ed77b122dd1fbb9a6a5373da36ab32d0f0cee1.
    The noisy audio playback on Tegra3 with secure-OS issue is fixed by
    updating TL's SDK to 1.08. Change min_rate for clocks back to 12Mhz.
    
    Bug 939415
    
    Change-Id: Ib0daf98faa85cc7cbc1dbfd4bf458427f914d830
    Signed-off-by: Hunk Lin <hulin@nvidia.com>
    Reviewed-on: http://git-master/r/104317
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Wen Yi <wyi@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra3_clocks.c
+++ arch/arm/mach-tegra/tegra3_clocks.c
@@ -3969,7 +3969,7 @@ static struct clk tegra_clk_sclk = {
 	.reg	= 0x28,
 	.ops	= &tegra_super_ops,
 	.max_rate = 378000000,
-	.min_rate = 40000000,
+	.min_rate = 12000000,
 };
 
 static struct clk tegra_clk_virtual_cpu_g = {
@@ -4024,7 +4024,7 @@ static struct clk tegra_clk_hclk = {
 	.reg_shift	= 4,
 	.ops		= &tegra_bus_ops,
 	.max_rate       = 378000000,
-	.min_rate       = 40000000,
+	.min_rate       = 12000000,
 };
 
 static struct clk tegra_clk_pclk = {
@@ -4035,7 +4035,7 @@ static struct clk tegra_clk_pclk = {
 	.reg_shift	= 0,
 	.ops		= &tegra_bus_ops,
 	.max_rate       = 167000000,
-	.min_rate       = 40000000,
+	.min_rate       = 12000000,
 };
 
 static struct raw_notifier_head sbus_rate_change_nh;
