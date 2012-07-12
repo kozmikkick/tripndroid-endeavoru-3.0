5b9b920acaada8c252c94845a354af5d2cd26086
TripNRaVeR <noerialexeew@live.nl>
2012-07-13 01:33:16 +0200

    arm: tegra: fix baseband files for endeavoru

--- arch/arm/mach-tegra/baseband-xmm-power.h
+++ arch/arm/mach-tegra/baseband-xmm-power.h
@@ -22,6 +22,22 @@
 
 #define VENDOR_ID         0x1519
 #define PRODUCT_ID        0x0020
+
+#if defined(CONFIG_MACH_EDGE)\
+	|| defined(CONFIG_MACH_EDGE_TD)\
+	|| defined(CONFIG_MACH_BLUE)\
+	|| defined(CONFIG_MACH_QUATTRO_U)\
+	|| defined(CONFIG_MACH_ENDEAVORU)
+
+#include <linux/completion.h>
+#include "gpio-names.h"
+
+#define BB_XMM_OEM1
+
+#endif
+
+#define VENDOR_ID         0x1519
+#define PRODUCT_ID        0x0020
 #define TEGRA_EHCI_DEVICE "/sys/devices/platform/tegra-ehci.1/ehci_power"
 
 #define XMM_MODEM_VER_1121	0x1121
@@ -71,19 +87,24 @@ enum baseband_xmm_power_work_state_t {
 	BBXMM_WORK_INIT_FLASH_STEP1,
 	/* initialize flash (with power management support) modem */
 	BBXMM_WORK_INIT_FLASH_PM_STEP1,
+	BBXMM_WORK_INIT_FLASH_PM_VER_LT_1130_STEP1,
+	BBXMM_WORK_INIT_FLASH_PM_VER_GE_1130_STEP1,
 	/* initialize flashless (with power management support) modem */
 	BBXMM_WORK_INIT_FLASHLESS_PM_STEP1,
-	BBXMM_WORK_INIT_FLASHLESS_PM_STEP2,
-	BBXMM_WORK_INIT_FLASHLESS_PM_STEP3,
-	BBXMM_WORK_INIT_FLASHLESS_PM_STEP4,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_LT_1130_WAIT_IRQ,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_LT_1130_STEP1,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_LT_1130_STEP2,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_GE_1130_STEP1,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_GE_1130_STEP2,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_GE_1130_STEP3,
+	BBXMM_WORK_INIT_FLASHLESS_PM_VER_GE_1130_STEP4,
 };
 
-struct xmm_power_data {
+struct baseband_xmm_power_work_t {
+	/* work structure must be first structure member */
+	struct work_struct work;
 	/* xmm modem state */
 	enum baseband_xmm_power_work_state_t state;
-	struct baseband_power_platform_data *pdata;
-	struct work_struct work;
-	struct platform_device *hsic_device;
 };
 
 enum baseband_xmm_powerstate_t {
@@ -99,9 +120,8 @@ enum baseband_xmm_powerstate_t {
 	BBXMM_PS_LAST	= -1,
 };
 
-irqreturn_t xmm_power_ipc_ap_wake_irq(int irq, void *dev_id);
+irqreturn_t baseband_xmm_power_ipc_ap_wake_irq(int irq, void *dev_id);
 
 void baseband_xmm_set_power_status(unsigned int status);
-extern struct xmm_power_data xmm_power_drv_data;
 
 #endif  /* BASREBAND_XMM_POWER_H */
