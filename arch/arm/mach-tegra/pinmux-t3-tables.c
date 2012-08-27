e1d7914e1e0a981975b6463d700ed68227c09271
TripNRaVeR <noerialexeew@live.nl>
2012-08-27 22:49:58 +0200

    pinmux-t3-tables: disable DAP1 when building for endeavoru

--- arch/arm/mach-tegra/pinmux-t3-tables.c
+++ arch/arm/mach-tegra/pinmux-t3-tables.c
@@ -465,7 +465,9 @@ static struct syscore_ops tegra_pinmux_syscore_ops = {
 
 static __initdata struct tegra_drive_pingroup_config t30_def_drive_pinmux[] = {
 	SET_DRIVE(DAP2, DISABLE, ENABLE, DIV_1, 31, 31, FASTEST, FASTEST),
+#ifndef CONFIG_MACH_ENDEAVORU
 	SET_DRIVE(DAP1, DISABLE, ENABLE, DIV_1, 31, 31, FASTEST, FASTEST),
+#endif
 };
 
 void __init tegra_init_pinmux(void)
