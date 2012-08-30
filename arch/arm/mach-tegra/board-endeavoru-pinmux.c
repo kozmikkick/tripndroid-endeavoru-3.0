e710e1af8854d58863f6d40c84b25183d38cc994
TripNRaVeR <noerialexeew@live.nl>
2012-08-31 01:45:42 +0200

    board-endeavoru-pinmux: configure LCD_D21 to RSVD2

--- arch/arm/mach-tegra/board-endeavoru-pinmux.c
+++ arch/arm/mach-tegra/board-endeavoru-pinmux.c
@@ -238,7 +238,7 @@ static __initdata struct tegra_pingroup_config endeavoru_pinmux_common[] = {
     DEFAULT_PINMUX(LCD_D18,         RSVD,            NORMAL,       NORMAL,     INPUT), // CAM_FRONT_CAM_RST#
     DEFAULT_PINMUX(LCD_D19,         DISPLAYA,        NORMAL,       NORMAL,     INPUT), // NC
     DEFAULT_PINMUX(LCD_D20,         DISPLAYA,        NORMAL,       NORMAL,     INPUT), // MDM_V_DCIN_MODEM_EN
-    DEFAULT_PINMUX(LCD_D21,         RSVD,            NORMAL,       NORMAL,     OUTPUT), // NFC_VEN
+    DEFAULT_PINMUX(LCD_D21,         RSVD2,           NORMAL,       NORMAL,     OUTPUT), // NFC_VEN
     DEFAULT_PINMUX(LCD_D22,         RSVD2,           NORMAL,       NORMAL,     OUTPUT), // NFC_DL_MODE
     DEFAULT_PINMUX(LCD_D23,         RSVD2,           NORMAL,       NORMAL,     INPUT), // CAM_V_CAM_VAA_2V85_EN
 
